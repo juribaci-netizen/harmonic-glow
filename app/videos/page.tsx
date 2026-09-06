d109829ebb84af39b3adb30518b7bc434746e3a7
juribaci-netizen <juribaci@gmail.com>
2026-09-06 21:39:40 +0200

    Wire concert archive sync into videos page

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,6 +1,6 @@
 import { AppShell } from "@/components/app-shell"
 import { VideosView } from "@/components/videos-view"
-import { getVideos, seedVideos } from "@/app/actions/videos"
+import { getVideos, seedVideos, syncConcertArchive } from "@/app/actions/videos"
 import { getSessionUser } from "@/lib/session"
 import { redirect } from "next/navigation"
 
@@ -8,5 +8,5 @@ export default async function VideosPage() {
   const user = await getSessionUser()
   if (!user) redirect("/sign-in")
   const videos = await getVideos()
-  return <AppShell user={{ name: user.name, email: user.email }}><VideosView initialVideos={videos} seedVideos={seedVideos} /></AppShell>
+  return <AppShell user={{ name: user.name, email: user.email }}><VideosView initialVideos={videos} seedVideos={seedVideos} syncArchive={syncConcertArchive} /></AppShell>
 }
