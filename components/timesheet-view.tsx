b7db9a597da30667f032a7cfed38875d543c2c31
juribaci-netizen <juribaci@gmail.com>
2026-09-09 14:46:48 +0200

    Align signature start above Podpis label

--- components/timesheet-view.tsx
+++ components/timesheet-view.tsx
@@ -190,7 +190,7 @@ export function TimesheetView({initialEntries,year:initialYear,month:initialMont
                   {signed&&signatureData&&<img
                     src={signatureData}
                     alt="Podpis zamestnanca"
-                    className="absolute left-[63.5%] top-[87.7%] h-[4.2%] w-[22%] object-contain"
+                    className="absolute left-[61.5%] top-[86.9%] h-[4.2%] w-[22%] object-contain"
                   />}
                   {visibleDays.flatMap(([date,es])=>{
                     const day=Number(date.slice(-2))
